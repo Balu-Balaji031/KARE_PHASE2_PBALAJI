0aea24d1c4c2657b89099174d9c42b45bfefe63e
P Balaji <balajipathakamurl@gmail.com>
2026-08-22 16:03:12 +0530

    Replace combined Day 1 practice with 10 separate Java programs

--- Day 1 - 10 Solved Problems.java
+++ Day 1 - 10 Solved Problems.java
@@ -1,16 +1,42 @@
-import java.util.*;
+// DAY 1 - PATTERNS
+// Each problem below is a separate, independently compilable Java program.
 
-// Day 1 - Pattern Practice: 10 solved problems
-public class Day1Solved {
-    static void hollowRectangle(int r,int c){for(int i=0;i<r;i++){for(int j=0;j<c;j++)System.out.print(i==0||i==r-1||j==0||j==c-1?'*':' ');System.out.println();}}
-    static void rightTriangle(int n){for(int i=1;i<=n;i++){for(int j=1;j<=i;j++)System.out.print('*');System.out.println();}}
-    static void invertedTriangle(int n){for(int i=n;i>=1;i--){for(int j=1;j<=i;j++)System.out.print('*');System.out.println();}}
-    static void pyramid(int n){for(int i=1;i<=n;i++){for(int s=1;s<=n-i;s++)System.out.print(' ');for(int j=1;j<=2*i-1;j++)System.out.print('*');System.out.println();}}
-    static void invertedPyramid(int n){for(int i=n;i>=1;i--){for(int s=1;s<=n-i;s++)System.out.print(' ');for(int j=1;j<=2*i-1;j++)System.out.print('*');System.out.println();}}
-    static void diamond(int n){pyramid(n);invertedPyramid(n-1);}
-    static void hollowPyramid(int n){for(int i=1;i<=n;i++){for(int s=1;s<=n-i;s++)System.out.print(' ');for(int j=1;j<=2*i-1;j++)System.out.print(i==n||j==1||j==2*i-1?'*':' ');System.out.println();}}
-    static void numberTriangle(int n){for(int i=1;i<=n;i++){for(int j=1;j<=i;j++)System.out.print(j);System.out.println();}}
-    static void checker(int n){for(int i=0;i<n;i++){for(int j=0;j<n;j++)System.out.print((i+j)%2);System.out.println();}}
-    static void hollowDiagonalSquare(int n){for(int i=0;i<n;i++){for(int j=0;j<n;j++)System.out.print(i==0||j==0||i==n-1||j==n-1||i==j||i+j==n-1?'*':' ');System.out.println();}}
-    public static void main(String[] args){hollowRectangle(4,7);rightTriangle(5);invertedTriangle(5);pyramid(4);invertedPyramid(4);diamond(4);hollowPyramid(5);numberTriangle(5);checker(5);hollowDiagonalSquare(5);}
-}
+// Problem 1: Solid square
+import java.util.*;
+class Problem1 { public static void main(String[] args){ Scanner sc=new Scanner(System.in); int n=sc.nextInt(); for(int i=0;i<n;i++){for(int j=0;j<n;j++)System.out.print("* ");System.out.println();} } }
+
+// Problem 2: Increasing triangle
+import java.util.*;
+class Problem2 { public static void main(String[] args){ Scanner sc=new Scanner(System.in); int n=sc.nextInt(); for(int i=1;i<=n;i++){for(int j=1;j<=i;j++)System.out.print("* ");System.out.println();} } }
+
+// Problem 3: Decreasing triangle
+import java.util.*;
+class Problem3 { public static void main(String[] args){ Scanner sc=new Scanner(System.in); int n=sc.nextInt(); for(int i=n;i>=1;i--){for(int j=1;j<=i;j++)System.out.print("* ");System.out.println();} } }
+
+// Problem 4: Right aligned triangle
+import java.util.*;
+class Problem4 { public static void main(String[] args){ Scanner sc=new Scanner(System.in); int n=sc.nextInt(); for(int i=1;i<=n;i++){for(int s=1;s<=n-i;s++)System.out.print("  ");for(int j=1;j<=i;j++)System.out.print("* ");System.out.println();} } }
+
+// Problem 5: Number triangle
+import java.util.*;
+class Problem5 { public static void main(String[] args){ Scanner sc=new Scanner(System.in); int n=sc.nextInt(); for(int i=1;i<=n;i++){for(int j=1;j<=i;j++)System.out.print(j+" ");System.out.println();} } }
+
+// Problem 6: Same number in each row
+import java.util.*;
+class Problem6 { public static void main(String[] args){ Scanner sc=new Scanner(System.in); int n=sc.nextInt(); for(int i=1;i<=n;i++){for(int j=1;j<=i;j++)System.out.print(i+" ");System.out.println();} } }
+
+// Problem 7: Inverted number triangle
+import java.util.*;
+class Problem7 { public static void main(String[] args){ Scanner sc=new Scanner(System.in); int n=sc.nextInt(); for(int i=n;i>=1;i--){for(int j=1;j<=i;j++)System.out.print(j+" ");System.out.println();} } }
+
+// Problem 8: 0-1 checkerboard
+import java.util.*;
+class Problem8 { public static void main(String[] args){ Scanner sc=new Scanner(System.in); int n=sc.nextInt(); for(int i=0;i<n;i++){for(int j=0;j<n;j++)System.out.print((i+j)%2==0?"0 ":"1 ");System.out.println();} } }
+
+// Problem 9: X pattern
+import java.util.*;
+class Problem9 { public static void main(String[] args){ Scanner sc=new Scanner(System.in); int n=sc.nextInt(); for(int i=0;i<n;i++){for(int j=0;j<n;j++)System.out.print(i==j||i+j==n-1?"* ":"  ");System.out.println();} } }
+
+// Problem 10: Hollow square
+import java.util.*;
+class Problem10 { public static void main(String[] args){ Scanner sc=new Scanner(System.in); int n=sc.nextInt(); for(int i=0;i<n;i++){for(int j=0;j<n;j++)System.out.print(i==0||j==0||i==n-1||j==n-1?"* ":"  ");System.out.println();} } }
